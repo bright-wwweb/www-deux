d0ca6c5b079577a82906865e1bf4585438195cb0
karinamzalez <karinamzalez@gmail.com>
2019-08-23 00:52:12 -0600

    add compost take home, transpo take home WIP

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from '../components/Layout';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import pic1 from '../assets/images/pic01.jpg';
+import compost from '../assets/images/compost.jpeg';
 import pic2 from '../assets/images/pic02.png';
 import Scroll from '../components/Scroll';
 import Gallery from '../components/Gallery';
@@ -75,18 +76,35 @@ const IndexPage = () => (
 
     <article id="first" className="container box style1 right">
       <a href="/#" className="image fit">
-        <img src={pic1} alt="" />
+        <img src={compost} />
       </a>
       <div className="inner">
         <header>
           <h2>
             RE: 
             <br />
-            Recycling
+            I Don't Compost
           </h2>
         </header>
+        <h4>
+          Where Are We Now?
+        </h4>
+        <p><a href="https://archive.epa.gov/epawaste/nonhaz/municipal/web/html/">Compostable waste (food scraps & yard waste) make up about 30% of what we throw away.</a>When recyclable/compostable materials aren't disposed of properly, they produce millions of metric tons of carbon dioxide and expedite the global warming. 🥵</p>
+        <h4>
+          Where Do We Need To Be?
+        </h4>
         <p>
-          This is an action item
+          <a href="https://www.epa.gov/sustainable-management-food/reducing-impact-wasted-food-feeding-soil-and-composting">Recycling and composting keeps our air clean!🥰🌳</a> According to the Composting Council, if everyone in the United States composted all of their food waste, the impact would be equivalent to removing 7.8 million cars from the road.
+        </p>
+        <h4>
+          Daily Action Item: 
+        </h4>
+        <p>
+          Opt in to Denver’s <a href="https://www.denvergov.org/content/denvergov/en/trash-and-recycling/composting/compost-collection-program.html">Compost Collection Program ($10 per month)</a>
+          <br />
+          OR 
+          <br />
+          DIY IT: <a href="https://learn.eartheasy.com/guides/composting/">Composting for Beginners</a>
         </p>
       </div>
     </article>
@@ -103,9 +121,30 @@ const IndexPage = () => (
             Driving
           </h2>
         </header>
+        <h4>
+          Where Are We Now?
+        </h4>
+        <p><a href="https://www.epa.gov/transportation-air-pollution-and-climate-change/carbon-pollution-transportation">Greenhouse gas (GHG) emissions from transportation account for about 29 percent of total U.S. greenhouse gas emissions</a>, making it the largest contributor of U.S. GHG emissions. Burning one gallon of gasoline creates about 20 pounds of CO2—which means the average vehicle creates roughly 6 to 9 tons of CO2 each year. 🚙🤕</p>
+        <h4>
+          Where Do We Need To Be?
+        </h4>
         <p>
-          This is an action item 
+          <a href="https://www.c2es.org/content/reducing-your-transportation-footprint/">Public and Analogue Transpo keeps our air breathable!🚲🥰</a> Communities with strong public transportation can reduce the nation’s carbon emissions by 37 million metric tons yearly. To achieve a similar reduction in carbon emissions, every household in New York City, Washington, D.C., Atlanta, Denver and Los Angeles combined would have to completely stop using electricity.🕯 Over the average lifetime of an American car, a 30-mpg car will save roughly $3,000 in fuel costs compared with a 20-mpg car.🤑 Just switching from a vehicle that gets 20-mpg to a vehicle that gets 25-mpg car reduces your greenhouse gas emissions by 1.7 tons annually. 🌎🙌🏽
         </p>
+        <h4>
+          Daily Action Item:
+        </h4>
+        <ol>
+          Opt in to Denver’s <a href="https://www.denvergov.org/content/denvergov/en/trash-and-recycling/composting/compost-collection-program.html">
+            <li>Walk or bike when you can. </li>
+          </a>
+          <li>Take public transit when possible. </li>
+          <li>Carpool with friends instead of driving alone. </li>
+          <li>Drive efficiently – go easy on the gas pedal and brakes AND get regular tune-ups.</li>
+          <li><a href="https://www.fueleconomy.gov/">Choose efficient cars</a></li>
+          <li>When getting home deliveries or shopping online, consider asking to have all your packages sent in one shipment and with minimal packaging.📦</li>
+          <li>Use a manual (reel) mower for small lawns.</li>
+        </ol>
       </div>
     </article>
 
